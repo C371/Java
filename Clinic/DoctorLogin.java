8032675a0e46f2130817fb90c98ff0754038c596
Niko <kokbisaniko@gmail.com>
2025-06-18 11:38:44 +0700

    finished, program

--- Clinic/DoctorLogin.java
+++ Clinic/DoctorLogin.java
@@ -1,8 +1,8 @@
 // import java.io.BufferedReader;
 // import java.io.FileReader;
+import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.time.LocalDateTime;
 
 class Doctor {
